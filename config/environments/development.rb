a512d4dafa35d58a85804d4e75c8c354e24734eb
sneeu-leeu <sa070197@gmail.com>
2022-05-30 16:46:16 +0200

    Add devise to users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,9 +24,7 @@ Rails.application.configure do
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
+    config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
 
@@ -67,4 +65,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  #
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
