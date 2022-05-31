45ca7fe4ace65a9aa71f435bf8cf0a686844b553
sneeu-leeu <sa070197@gmail.com>
2022-05-31 10:19:24 +0200

    Displays the user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  root to: "users#show"
+
+  resources :users, only: %i[show]
 end
