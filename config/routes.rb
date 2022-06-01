eb21307f3e27f0297dc7b1aed2cc9bbb1cabaabf
sneeu-leeu <sa070197@gmail.com>
2022-06-01 13:44:17 +0200

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "users#show"
+  root to: "welcome#index"
 
   resources :users, only: %i[show]
+  resources :welcome, only: %i[index info start]
 end
